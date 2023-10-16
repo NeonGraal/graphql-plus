88a99190cd444286050d514e4580194b03058c87
Struan Judd <git@scifi.geek.nz>
2023-10-17 07:58:13 +1300

    Add GraphQl Example verification

---
+++ test/GqlPlus.Verifier.ClassTests/Operation/VerifyGraphQlExamples.cs
@@ -0,0 +1,32 @@
+﻿using GqlPlus.Verifier.Ast;
+
+namespace GqlPlus.Verifier.Operation;
+
+[UsesVerify]
+public class VerifyGraphQlExamples
+{
+  [Theory]
+  [ClassData(typeof(GraphQlExamplesData))]
+  public Task VerifyExample(string example)
+  {
+    var operation = File.ReadAllText("GraphQl/Example_" + example + ".gql");
+    Tokenizer tokenizer = new(operation);
+    OperationParser parser = new(tokenizer);
+    OperationAst ast = parser.Parse();
+
+    var settings = new VerifySettings();
+    settings.UseTextForParameters(example);
+    settings.ScrubEmptyLines();
+
+    return Verify(ast.Render(), settings);
+  }
+
+  public class GraphQlExamplesData : TheoryData<string>
+  {
+    public GraphQlExamplesData()
+    {
+      Add("003");
+      Add("005");
+    }
+  }
+}

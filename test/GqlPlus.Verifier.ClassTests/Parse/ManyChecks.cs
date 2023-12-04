95ecd7826b7887e268e4411ed2b956b8af588361
Struan Judd <git@scifi.geek.nz>
2023-12-05 08:41:38 +1300

    Tidy and support .Net 8

--- test/GqlPlus.Verifier.ClassTests/Parse/ManyChecks.cs
+++ test/GqlPlus.Verifier.ClassTests/Parse/ManyChecks.cs
@@ -1,47 +1,5 @@
 ﻿namespace GqlPlus.Verifier.Parse;
 
-internal sealed class ManyChecks<T>
-{
-  private readonly IParserArray<T> _parser;
-  private readonly string _type = typeof(T).ToString();
-
-  public ManyChecks(IParserArray<T> parser)
-    => _parser = parser;
-
-  internal void TrueExpected(string input, bool skipIf, params T[] expected)
-  {
-    if (!skipIf) {
-      TrueExpected(input, expected);
-    }
-  }
-
-  internal void TrueExpected(string input, params T[] expected)
-  {
-    var result = _parser.Parse(Tokens(input), _type);
-
-    result.IsOk().Should().BeTrue(_type);
-    using var scope = new AssertionScope();
-    result.Required().Should().Equal(expected);
-  }
-
-  internal void False(string input)
-  {
-    var result = _parser.Parse(Tokens(input), _type);
-
-    using var scope = new AssertionScope();
-    result.IsError(message => message.Message.Contains("Expected")).Should().BeTrue(_type);
-  }
-
-  internal void Count(string input, int count)
-  {
-    var result = _parser.Parse(Tokens(input), _type);
-
-    result.IsOk().Should().BeTrue(_type);
-    using var scope = new AssertionScope();
-    result.Required().Length.Should().Be(count);
-  }
-}
-
 internal sealed class ManyChecksParser<T>
 {
   private readonly Parser<T>.LA _parser;
--- test/GqlPlus.Verifier.ClassTests/Parse/OneChecks.cs
+++ test/GqlPlus.Verifier.ClassTests/Parse/OneChecks.cs
@@ -1,99 +1,5 @@
 ﻿namespace GqlPlus.Verifier.Parse;
 
-internal class OneChecks<T>
-{
-  private readonly IParser<T> _parser;
-  private readonly string _type = typeof(T).ToString();
-
-  public OneChecks(IParser<T> parser)
-    => _parser = parser;
-
-  internal void TrueExpected(string input, T expected, bool skipIf = false)
-  {
-    if (skipIf) {
-      return;
-    }
-
-    var tokens = Tokens(input);
-    var result = _parser.Parse(tokens);
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsOk().Should().BeTrue(_type + " -> " + input);
-    scope.FormattingOptions.MaxDepth = 10;
-    tokens.Errors.Should().BeEmpty();
-    result.Required().Should().Be(expected);
-  }
-
-  internal void Ok(string input, T expected, bool skipIf = false)
-  {
-    if (skipIf) {
-      return;
-    }
-
-    var tokens = Tokens(input);
-    var result = _parser.Parse(tokens);
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsOk().Should().BeTrue(_type + " -> " + input);
-    tokens.Errors.Should().BeEmpty();
-    result.Required().Should().Be(expected);
-  }
-
-  internal void Empty(string input)
-  {
-    var tokens = Tokens(input);
-    var result = _parser.Parse(tokens);
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsEmpty().Should().BeTrue(_type + " -> " + input);
-    tokens.Errors.Should().BeEmpty();
-  }
-
-  internal void Partial(string input, string error, T expected, bool skipIf = false)
-  {
-    if (skipIf) {
-      return;
-    }
-
-    var result = _parser.Parse(Tokens(input));
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsError(message => message.Message.Contains(error)).Should().BeTrue(_type + " -> " + input);
-    result.Optional().Should().Be(expected);
-  }
-
-  internal void Error(string input, string error, bool skipIf = false)
-  {
-    if (skipIf) {
-      return;
-    }
-
-    var result = _parser.Parse(Tokens(input));
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsError(message => message.Message.Contains(error)).Should().BeTrue(_type + " -> " + input);
-  }
-
-  internal void False(string input, Action<T?>? check = null, bool skipIf = false)
-  {
-    if (skipIf) {
-      return;
-    }
-
-    var result = _parser.Parse(Tokens(input));
-
-    using var scope = new AssertionScope();
-    scope.FormattingOptions.MaxDepth = 10;
-    result.IsError(message => message.Message.Contains("Expected")).Should().BeTrue(_type + " -> " + input);
-    result.Optional(result => check?.Invoke(result));
-  }
-}
-
 internal class OneChecksParser<T>
 {
   private readonly Parser<T>.L _parser;
@@ -188,7 +94,7 @@ internal class OneChecksParser<T>
   }
 }
 
-internal class OneChecksParser<I, T>
+internal sealed class OneChecksParser<I, T>
   where I : Parser<T>.I
 {
   private readonly Parser<I, T>.L _parser;

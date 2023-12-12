6092790b2174ee9ed9ad36180eaedcbc2dccbe5d
Struan Judd <git@scifi.geek.nz>
2023-12-12 13:11:18 +1300

    Improve MergeOutputFields

--- src/GqlPlus.Verifier/Merging/MergeOutputFields.cs
+++ src/GqlPlus.Verifier/Merging/MergeOutputFields.cs
@@ -2,7 +2,11 @@
 
 namespace GqlPlus.Verifier.Merging;
 
-internal class MergeOutputFields
+internal class MergeOutputFields(IMerge<ParameterAst> parameters)
   : FieldsMerger<OutputFieldAst, OutputReferenceAst>
 {
+  public override bool CanMerge(OutputFieldAst[] items)
+    => base.CanMerge(items)
+      && items.CanMerge(item => item.EnumValue ?? "-")
+      && parameters.CanMerge([.. items.SelectMany(item => item.Parameters)]);
 }
--- test/GqlPlus.Verifier.ClassTests/Merging/MergeOutputFieldsTests.cs
+++ test/GqlPlus.Verifier.ClassTests/Merging/MergeOutputFieldsTests.cs
@@ -1,12 +1,67 @@
 ﻿using GqlPlus.Verifier.Ast;
 using GqlPlus.Verifier.Ast.Schema;
+using NSubstitute;
 
 namespace GqlPlus.Verifier.Merging;
 
 public class MergeOutputFieldsTests
   : TestFields<OutputFieldAst, OutputReferenceAst>
 {
-  private readonly MergeOutputFields _merger = new();
+  [Theory, RepeatData(Repeats)]
+  public void CanMerge_TwoItemsParametersCantMerge_ReturnsFalse(string name, string type)
+  {
+    var items = new[] { MakeField(name, type), MakeField(name, type) };
+    _parameters.CanMerge([]).ReturnsForAnyArgs(false);
+
+    var result = _merger.CanMerge(items);
+
+    result.Should().BeFalse();
+  }
+
+  [Theory, RepeatData(Repeats)]
+  public void CanMerge_TwoItemsSameEnum_ReturnsTrue(string name, string type, string value)
+  {
+    var items = new[] { MakeField(name, type) with { EnumValue = value }, MakeField(name, type) with { EnumValue = value } };
+
+    var result = _merger.CanMerge(items);
+
+    result.Should().BeTrue();
+  }
+
+  [Theory, RepeatData(Repeats)]
+  public void CanMerge_TwoItemsDifferentEnums_ReturnsFalse(string name, string type, string value1, string value2)
+  {
+    if (value1 == value2) {
+      return;
+    }
+
+    var items = new[] { MakeField(name, type) with { EnumValue = value1 }, MakeField(name, type) with { EnumValue = value2 } };
+
+    var result = _merger.CanMerge(items);
+
+    result.Should().BeFalse();
+  }
+
+  [Theory, RepeatData(Repeats)]
+  public void CanMerge_TwoItemsOneEnum_ReturnsFalse(string name, string type, string value)
+  {
+    var items = new[] { MakeField(name, type) with { EnumValue = value }, MakeField(name, type) };
+
+    var result = _merger.CanMerge(items);
+
+    result.Should().BeFalse();
+  }
+
+  private readonly MergeOutputFields _merger;
+  private readonly IMerge<ParameterAst> _parameters;
+
+  public MergeOutputFieldsTests()
+  {
+    _parameters = Substitute.For<IMerge<ParameterAst>>();
+    _parameters.CanMerge([]).ReturnsForAnyArgs(true);
+
+    _merger = new(_parameters);
+  }
 
   protected override FieldsMerger<OutputFieldAst, OutputReferenceAst> MergerField => _merger;
 

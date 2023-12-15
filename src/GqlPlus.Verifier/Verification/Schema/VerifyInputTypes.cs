d0e698804406c7eb6ea0db0e8aa7805414a38e20
Struan Judd <git@scifi.geek.nz>
2023-12-16 12:24:11 +1300

    Improve Input Type verification

--- src/GqlPlus.Verifier/Verification/Schema/VerifyInputTypes.cs
+++ src/GqlPlus.Verifier/Verification/Schema/VerifyInputTypes.cs
@@ -1,4 +1,5 @@
-﻿using GqlPlus.Verifier.Ast.Schema;
+﻿using GqlPlus.Verifier.Ast;
+using GqlPlus.Verifier.Ast.Schema;
 using GqlPlus.Verifier.Token;
 
 namespace GqlPlus.Verifier.Verification.Schema;
@@ -7,7 +8,20 @@ internal class VerifyInputTypes(
   IVerifyAliased<InputDeclAst> aliased
 ) : UsageAliasedVerifier<InputDeclAst, AstType>(aliased)
 {
+  protected override ITokenMessages UsageValue(InputDeclAst usage, IMap<AstType[]> byId)
+  {
+    var errors = new TokenMessages();
 
-  //  protected override string UsageKey(InputAst item) => item.Extends?.Name ?? "";
-  protected override ITokenMessages UsageValue(InputDeclAst usage, IMap<AstType[]> byId) => new TokenMessages();
+    foreach (var field in usage.Fields) {
+      if (!byId.ContainsKey(field.Type.Name)) {
+        errors.Add(usage.Error($"Invalid Input Field. '{field.Type}' not defined."));
+      }
+
+      if (field.Default?.Value?.EnumValue == "Null.null" && !(field.Modifiers.LastOrDefault()?.Kind == ModifierKind.Optional)) {
+        errors.Add(usage.Error($"Invalid Input Field Default. 'null' default requires Optional type, not '{field.ModifiedType}'."));
+      }
+    }
+
+    return errors;
+  }
 }

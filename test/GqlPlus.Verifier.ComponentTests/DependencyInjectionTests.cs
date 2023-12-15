24723e45a917030b6c868ad3711fdd85d70f2ff8
Struan Judd <git@scifi.geek.nz>
2023-12-15 23:19:01 +1300

    Improve DI tests

--- test/GqlPlus.Verifier.ComponentTests/DependencyInjectionTests.cs
+++ test/GqlPlus.Verifier.ComponentTests/DependencyInjectionTests.cs
@@ -1,25 +1,33 @@
 ﻿using System.Text;
 using Microsoft.Extensions.DependencyInjection;
+using Xunit.DependencyInjection;
 
 namespace GqlPlus.Verifier;
 
-[UsesVerify]
-public class DependencyInjectionTests(IServiceCollection services)
+public class DependencyInjectionTests(IServiceCollection services, ITestOutputHelperAccessor output)
 {
   [Fact]
-  public async Task ParseSampleGraphQl()
+  public void CheckDependencyInjectionContainer()
   {
     StringBuilder sb = new();
     var sds = new List<ServiceDescriptor>(services)
         .OrderBy(o => o.ServiceType.ExpandTypeName());
 
+    var hashset = sds.Select(o => o.ServiceType.FullTypeName()).ToHashSet();
+
+    using var scope = new AssertionScope();
+
     foreach (ServiceDescriptor sd in sds) {
+      sb.Clear();
+
       sb.Append(sd.ServiceType.FullTypeName());
       sb.Append(", ");
       sb.Append(sd.Lifetime);
       sb.Append(", ");
       if (sd.ImplementationType is not null) {
         sb.Append(sd.ImplementationType.FullTypeName(sd.ServiceType.Namespace));
+
+        CheckConstructor(sd.ImplementationType, hashset);
       } else if (sd.ImplementationFactory is not null) {
         sb.Append("() => ");
       } else if (sd.ImplementationInstance is not null) {
@@ -27,12 +35,36 @@ public class DependencyInjectionTests(IServiceCollection services)
         sb.Append(sd.ImplementationInstance.GetType().FullTypeName(sd.ServiceType.Namespace));
       }
 
-      sb.AppendLine();
+      output.Output?.WriteLine(sb.ToString());
     }
+  }
 
-    var settings = new VerifySettings();
-    settings.ScrubEmptyLines();
+  private static void CheckConstructor(Type implementationType, HashSet<string> hashset)
+  {
+    foreach (var ctor in implementationType.GetConstructors()) {
+      var missing = ctor.GetParameters()
+        .Where(p => !MatchType(hashset, p.ParameterType))
+        .Select(p => p.Name + ":" + p.ParameterType.ExpandTypeName()).ToList();
 
-    await Verify(sb, settings);
+      missing.Should().BeEmpty();
+    }
+  }
+
+  private static readonly HashSet<string> s_knownGenerics = [
+    "IConfigureOptions`1",
+    "ILogger`1",
+    "IOptions`1",
+    "IOptionsChangeTokenSource`1",
+    "IOptionsMonitor`1",
+    "IPostConfigureOptions`1",
+    "IValidateOptions`1",
+    "LoggerFilterOptions",
+  ];
+
+  private static bool MatchType(HashSet<string> hashset, Type parameterType)
+  {
+    return hashset.Contains(parameterType.FullTypeName())
+|| s_knownGenerics.Contains(parameterType.Name)
+|| parameterType.Name == "IEnumerable`1" && MatchType(hashset, parameterType.GenericTypeArguments[0]);
   }
 }

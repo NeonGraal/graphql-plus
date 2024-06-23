9baefcb76fb8312d2613df3f9b0ceba2fe503327
Struan Judd <git@scifi.geek.nz>
2024-06-24 07:31:22 +1200

    Tidy and improve OBject Ast tests

---
+++ test/GqlPlus.Parser.ClassTests/Ast/Schema/Objects/InputDeclAstTests.cs
@@ -0,0 +1,30 @@
+﻿using GqlPlus.Abstractions.Schema;
+
+namespace GqlPlus.Ast.Schema.Objects;
+
+public class InputDeclAstTests
+  : AstObjectTests
+{
+  private readonly InputDeclAstChecks _checks = new();
+
+  internal override IAstObjectChecks ObjectChecks => _checks;
+}
+
+internal sealed class InputDeclAstChecks
+  : AstObjectChecks<InputDeclAst, IGqlpInputBase, IGqlpInputField, IGqlpInputAlternate>
+{
+  public InputDeclAstChecks()
+    : base(dual => new InputDeclAst(AstNulls.At, dual),
+      parent => new InputBaseAst(AstNulls.At, parent))
+  { }
+
+  protected override string AlternateString(AlternateInput input)
+    => $"!IA {input.Type} [] ?";
+
+  protected override IGqlpInputAlternate[] CreateAlternates(IEnumerable<AlternateInput> alternates)
+    => alternates.InputAlternates();
+  protected override IGqlpInputField[] CreateFields(IEnumerable<FieldInput> fields)
+    => fields.InputFields();
+  protected override string FieldString(FieldInput input)
+    => $"!IF {input.Name} : {input.Type}";
+}

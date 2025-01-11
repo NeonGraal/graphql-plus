bd6c6b738e0fb1834401d7985ea10e3c44c5d5a4
Struan Judd <github@scifi.geek.nz>
2025-01-12 07:03:49 +1300

    Improve update script

--- update.ps1
+++ update.ps1
@@ -1,4 +1,11 @@
+$clean = -not (git status -s)
 dotnet outdated .\ -vl minor -u
+if ($clean) {
+    git commit -a -m "Update packages to latest patch version"
+    dotnet outdated .\ -vl major -u
+} else {
+    dotnet outdated .\ -vl major
+}
 dotnet outdated .\
 
 dotnet tools-outdated

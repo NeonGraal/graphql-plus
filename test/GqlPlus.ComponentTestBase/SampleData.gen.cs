db97121d91cf31320d0668878521cff9491c1c95
Struan Judd <github@scifi.geek.nz>
2025-03-07 07:42:18 +1300

    Correct Schema verify errors

--- test/GqlPlus.ComponentTestBase/SampleData.gen.cs
+++ test/GqlPlus.ComponentTestBase/SampleData.gen.cs
@@ -1,5 +1,5 @@
 ﻿// Generated from .\test\GqlPlus.ComponentTestBase\Samples
-// Collected from 8d27cdd  (HEAD -> correct-coverage, origin/correct-coverage) 2025-03-06 Further improve verify errors
+// Collected from 988fbb2  (HEAD -> type-constaints) 2025-03-07 Correct Schema verify errors
 
 namespace GqlPlus;
 

cdff9e1e321727bf984f09fac5ae7ae4f98767c3
Struan Judd <github@scifi.geek.nz>
2025-05-09 13:17:11 +1200

    Add Global Name parser class tests

---
+++ test/GqlPlus.Parser.ClassTests/Parsing/Schema/Globals/DirectiveNameTests.cs
@@ -0,0 +1,38 @@
+﻿namespace GqlPlus.Parsing.Schema.Globals;
+
+public class DirectiveNameTests
+  : ParserClassTestBase
+{
+  private readonly DirectiveName _directiveName = new();
+
+  [Theory, RepeatData]
+  public void ParseName_ShouldReturnTrue_WhenIdentifierIsValid(string input)
+  {
+    // Arrange
+    PrefixReturns('@', OutStringAt(input), OutFailAt());
+    IdentifierReturns(OutString(input));
+
+    // Act
+    bool result = _directiveName.ParseName(Tokenizer, out string? name, out TokenAt at);
+
+    // Assert
+    name.ShouldSatisfyAllConditions(
+      () => result.ShouldBeTrue(),
+      () => name.ShouldBe(input));
+  }
+
+  [Fact]
+  public void ParseName_ShouldReturnFalse_WhenIdentifierIsInvalid()
+  {
+    // Arrange
+    IdentifierReturns(OutFail);
+
+    // Act
+    bool result = _directiveName.ParseName(Tokenizer, out string? name, out TokenAt at);
+
+    // Assert
+    name.ShouldSatisfyAllConditions(
+      () => result.ShouldBeFalse(),
+      () => name.ShouldBeNull());
+  }
+}

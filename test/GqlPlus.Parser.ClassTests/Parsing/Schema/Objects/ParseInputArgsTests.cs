2df5d0b01bc7a93b442099b8cbd7337cdad2bc8a
Struan Judd <github@scifi.geek.nz>
2025-05-11 13:01:46 +1200

    Add Parser class tests for Input and Output schema objects

---
+++ test/GqlPlus.Parser.ClassTests/Parsing/Schema/Objects/ParseInputArgsTests.cs
@@ -0,0 +1,59 @@
+﻿using GqlPlus.Abstractions.Schema;
+
+namespace GqlPlus.Parsing.Schema.Objects;
+
+public class ParseInputArgsTests
+  : ParserClassTestBase
+{
+  private readonly ParseInputArgs _parser;
+
+  public ParseInputArgsTests()
+  {
+    _parser = new ParseInputArgs();
+
+    PrefixReturns('$', OutPass);
+  }
+
+  [Theory, RepeatData]
+  public void Parse_ShouldReturnInputArg_WhenValid(string argType)
+  {
+    // Arrange
+    TakeReturns('<', true);
+    IdentifierReturns(OutString(argType), OutFail);
+    TakeReturns('>', true);
+
+    // Act
+    IResultArray<IGqlpInputArg> result = _parser.Parse(Tokenizer, "testLabel");
+
+    // Assert
+    result.ShouldBeAssignableTo<IResultArrayOk<IGqlpInputArg>>();
+  }
+
+  [Fact]
+  public void Parse_ShouldReturnEmpty_WhenNoArgs()
+  {
+    // Arrange
+    TakeReturns('<', false);
+
+    // Act
+    IResultArray<IGqlpInputArg> result = _parser.Parse(Tokenizer, "testLabel");
+
+    // Assert
+    result.ShouldBeAssignableTo<IResultEmpty>();
+  }
+
+  [Fact]
+  public void Parse_ShouldReturnError_WhenInvalid()
+  {
+    // Arrange
+    TakeReturns('<', true);
+    IdentifierReturns(OutFail);
+    SetupError<IGqlpInputArg>();
+
+    // Act
+    IResultArray<IGqlpInputArg> result = _parser.Parse(Tokenizer, "testLabel");
+
+    // Assert
+    result.ShouldBeAssignableTo<IResultError>();
+  }
+}
---
+++ test/GqlPlus.Parser.ClassTests/Parsing/Schema/Objects/ParseOutputBaseTests.cs
@@ -0,0 +1,45 @@
+﻿using GqlPlus.Abstractions.Schema;
+
+namespace GqlPlus.Parsing.Schema.Objects;
+
+public class ParseOutputBaseTests
+  : ParserClassTestBase
+{
+  private readonly Parser<IGqlpOutputArg>.IA _parseArgs;
+  private readonly ParseOutputBase _parser;
+
+  public ParseOutputBaseTests()
+  {
+    Parser<IGqlpOutputArg>.DA parseArgs = ParserAFor(out _parseArgs);
+    _parser = new ParseOutputBase(parseArgs);
+
+    PrefixReturns('$', OutPass);
+  }
+
+  [Theory, RepeatData]
+  public void Parse_ShouldReturnOutputBase_WhenValid(string baseType)
+  {
+    // Arrange
+    IdentifierReturns(OutString(baseType));
+    ParseEmptyA(_parseArgs);
+
+    // Act
+    IResult<IGqlpOutputBase> result = _parser.Parse(Tokenizer, "testLabel");
+
+    // Assert
+    result.ShouldBeAssignableTo<IResultOk<IGqlpOutputBase>>();
+  }
+
+  [Fact]
+  public void Parse_ShouldReturnEmpty_WhenInvalid()
+  {
+    // Arrange
+    IdentifierReturns(OutFail);
+
+    // Act
+    IResult<IGqlpOutputBase> result = _parser.Parse(Tokenizer, "testLabel");
+
+    // Assert
+    result.ShouldBeAssignableTo<IResultEmpty>();
+  }
+}

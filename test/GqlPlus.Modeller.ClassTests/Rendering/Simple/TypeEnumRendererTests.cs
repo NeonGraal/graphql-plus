b90654c87eac0a19516f931f145c0a5e967c206a
Struan Judd <github@scifi.geek.nz>
2025-05-18 00:17:25 +1200

    Add class tests for TypeEnumRenderer and TypeUnionRenderer

---
+++ test/GqlPlus.Modeller.ClassTests/Rendering/Simple/TypeEnumRendererTests.cs
@@ -0,0 +1,28 @@
+﻿namespace GqlPlus.Rendering.Simple;
+
+public class TypeEnumRendererTests
+  : RendererClassTestBase<TypeEnumModel>
+{
+  private readonly ParentTypeRenderers<AliasedModel, EnumLabelModel> _renderers;
+
+  public TypeEnumRendererTests()
+  {
+    IRenderer<TypeRefModel<SimpleKindModel>> parent = RFor<TypeRefModel<SimpleKindModel>>();
+    IRenderer<AliasedModel> item = RFor<AliasedModel>();
+    IRenderer<EnumLabelModel> all = RFor<EnumLabelModel>();
+
+    _renderers = new ParentTypeRenderers<AliasedModel, EnumLabelModel>(parent, item, all);
+    Renderer = new TypeEnumRenderer(_renderers);
+  }
+
+  protected override IRenderer<TypeEnumModel> Renderer { get; }
+
+  [Theory, RepeatData]
+  public void Render_WithValidModel_ReturnsStructured(string name, string contents)
+    => RenderAndCheck(new(name, contents), [
+      "!_TypeEnum",
+      "description: " + contents.Quoted("'"),
+      "name: " + name,
+      "typeKind: !_TypeKind Enum"
+    ]);
+}
---
+++ test/GqlPlus.Modeller.ClassTests/Rendering/Simple/TypeUnionRendererTests.cs
@@ -0,0 +1,28 @@
+﻿namespace GqlPlus.Rendering.Simple;
+
+public class TypeUnionRendererTests
+  : RendererClassTestBase<TypeUnionModel>
+{
+  private readonly ParentTypeRenderers<AliasedModel, UnionMemberModel> _renderers;
+
+  public TypeUnionRendererTests()
+  {
+    IRenderer<TypeRefModel<SimpleKindModel>> parent = RFor<TypeRefModel<SimpleKindModel>>();
+    IRenderer<AliasedModel> item = RFor<AliasedModel>();
+    IRenderer<UnionMemberModel> all = RFor<UnionMemberModel>();
+
+    _renderers = new ParentTypeRenderers<AliasedModel, UnionMemberModel>(parent, item, all);
+    Renderer = new TypeUnionRenderer(_renderers);
+  }
+
+  protected override IRenderer<TypeUnionModel> Renderer { get; }
+
+  [Theory, RepeatData]
+  public void Render_WithValidModel_ReturnsStructured(string name, string contents)
+    => RenderAndCheck(new(name, contents), [
+      "!_TypeUnion",
+      "description: " + contents.Quoted("'"),
+      "name: " + name,
+      "typeKind: !_TypeKind Union"
+    ]);
+}

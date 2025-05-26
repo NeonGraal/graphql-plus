0101077dc23d68de37fc0421d32f85705f34b327
Struan Judd <github@scifi.geek.nz>
2025-05-27 06:48:47 +1200

    Improve Merging coverage and reduce CRAP scores

--- test/GqlPlus.Parser.ClassTests/Merging/Schema/MergeAllTypesTests.cs
+++ test/GqlPlus.Parser.ClassTests/Merging/Schema/MergeAllTypesTests.cs
@@ -1,4 +1,5 @@
 ﻿using GqlPlus.Abstractions.Schema;
+using GqlPlus.Ast.Schema.Objects;
 using GqlPlus.Ast.Schema.Simple;
 
 namespace GqlPlus.Merging.Schema;
@@ -6,6 +7,30 @@ namespace GqlPlus.Merging.Schema;
 public class MergeAllTypesTests
   : TestAbbreviatedMerger<IGqlpType>
 {
+  [Theory, RepeatData]
+  public void FixupType_WithEnums_FixesType(string enumType, string enumLabel, string fieldName, string domainName)
+  {
+    // Arrange
+    OutputFieldAst field = new(AstNulls.At, fieldName, new OutputBaseAst(AstNulls.At, "", "")) {
+      EnumLabel = enumLabel
+    };
+    DomainLabelAst domainLabel = new(AstNulls.At, "", false, enumLabel);
+
+    IGqlpType[] types = [
+      new EnumDeclAst(AstNulls.At, enumType, [new(AstNulls.At, enumLabel, "")]),
+      new OutputDeclAst(AstNulls.At, "Car") { ObjFields = [field] },
+      new AstDomain<DomainLabelAst, IGqlpDomainLabel>(AstNulls.At, domainName, DomainKind.Enum, [domainLabel]),
+    ];
+
+    // Act
+
+    _merger.Merge(types);
+
+    // Assert
+    field.BaseType.Name.ShouldBe(enumType);
+    domainLabel.EnumType.ShouldBe(enumType);
+  }
+
   private readonly MergeAllTypes _merger;
 
   public MergeAllTypesTests(ITestOutputHelper outputHelper)

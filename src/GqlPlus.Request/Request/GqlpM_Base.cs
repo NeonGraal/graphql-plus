7fa9b3eb7a86e4846f35db867ae2fd4ab4bfda98
Struan Judd <struan@scifi.geek.nz>
2026-04-22 07:28:34 +1200

    Refactor to GqlPlu.Schema project and add GqlPlus.Request project

---
+++ src/GqlPlus.Request/Request/GqlpM_Base.cs
@@ -0,0 +1,17 @@
+﻿namespace GqlPlus.Request;
+
+[GqlpGenerator(GqlpGeneratorType.Interface)]
+public interface IGqlpM_Base
+{ }
+
+[GqlpGenerator(GqlpGeneratorType.Model)]
+public class GqlpM_ModelBase
+{ }
+
+// [GqlpGenerator(GqlpGeneratorType.Encoder)]
+public class GqlpM_EncoderBase
+{ }
+
+// Todo: [GqlpGenerator(GqlpGeneratorType.Decoder)]
+public class GqlpM_DecoderBase
+{ }

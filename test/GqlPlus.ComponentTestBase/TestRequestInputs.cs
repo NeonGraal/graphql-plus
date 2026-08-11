83172e21d2a14969ca9cf590ca0a19298ac23c7f
Struan Judd <github@scifi.geek.nz>
2026-08-12 09:55:57 +1200

    Improve Request samples

--- test/GqlPlus.ComponentTestBase/TestRequestInputs.cs
+++ test/GqlPlus.ComponentTestBase/TestRequestInputs.cs
@@ -4,7 +4,7 @@ public abstract class TestRequestInputs
   : SampleChecks
 {
   [Theory]
-  [ClassData(typeof(SamplesRequestGreqData))]
+  [ClassData(typeof(SamplesRequestData))]
   public async Task Test_Request(string sample)
   {
     string request = await ReadRequest(sample);

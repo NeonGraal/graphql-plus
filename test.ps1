fe7ba7a2bc9f4e06a70ba633d8ea4a318f028819
Struan Judd <github@scifi.geek.nz>
2026-08-21 22:54:53 +1200

    Covert to using MTP

--- test.ps1
+++ test.ps1
@@ -4,30 +4,37 @@ param (
   $Filter = "",
   [ValidateSet('Html', 'Plain', 'Yaml', 'Json')]
   $Generate = "",
+  [switch]$NoBuild = $false,
   [switch]$ClassTests = $false,
   [switch]$Html = $false,
   $Framework = "10.0"
 )
 
-$test = "test","-e","GQLPLUS_TEST_LOGGING=1","--no-build"
-$test += "--logger","trx;LogFileName=TestResults-$Framework.trx","--framework","net$Framework"
-
-if ($Generate) {
-  $test += "--filter", "Generate=$Generate"
-} elseif ($Section) {
-  $test += "--filter", ".$Section."
-} elseif ($Filter) {
-  $test += "--filter", $Filter
-}
+$test = "test","-e","GQLPLUS_TEST_LOGGING=1","--framework","net$Framework","--no-build"
+# $test += "--logger","trx;LogFileName=TestResults-$Framework.trx","--framework","net$Framework"
 if ($ClassTests) {
   $test += @("GqlPlus.ClassTests.slnf")
 }
 
-dotnet build
+$test += "--","--report-xunit-trx","--report-xunit-trx-filename","TestResults-{tfm}.trx"
 
-if ($LASTEXITCODE -ne 0) {
-  Write-Host "Build failed, exiting."
-  exit $LASTEXITCODE
+if ($Generate) {
+  $test += "--trait-filter", "Generate=$Generate"
+}
+if ($Section) {
+  $test += "--namespace-filter", "*$Section*"
+}
+if ($Filter) {
+  $test += "--method-filter", "*$Filter*"
+}
+
+if (-not $NoBuild) {
+  dotnet build
+
+  if ($LASTEXITCODE -ne 0) {
+    Write-Host "Build failed, exiting."
+    exit $LASTEXITCODE
+  }
 }
 
 Get-ChildItem test -Filter 'TestResults' -Recurse -Directory | Remove-Item -Recurse -Force -ErrorAction Ignore
